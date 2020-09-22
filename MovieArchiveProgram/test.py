9d22254151aa10320f38cdf989625faa1543c9ee
hyperfitz <hyperfitz@users.noreply.github.com>
2020-09-22 10:11:44 -0700

    This is another test commit

--- MovieArchiveProgram/test.py
+++ MovieArchiveProgram/test.py
@@ -1,3 +1,8 @@
 """
 this is a test file
+
+This repository is for class projects I am working (have worked) on. It is intended to (hopefully) show skills I am working (have worked) to develop.
+
+Each directory is a different project that I have been assigned as coursework by an instructor.
+
 """
