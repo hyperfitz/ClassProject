bc91539ce9c856ae560f20826e8f32a8aa2eec92
hyperfitz <hyperfitz@users.noreply.github.com>
2020-09-23 10:29:01 -0700

    Added the movie add code to an overly verbose function.

--- MovieArchiveProgram/MovieArchive.py
+++ MovieArchiveProgram/MovieArchive.py
@@ -2,7 +2,7 @@
 This is a program that uses some functions to process user inputs for storing, listing, and searching for movies
 in a list of dictionaries. The movies are user-defined and are not stored once the program exits.
 
-As of September 22, 2020, I am still working on this one.
+As of September 23, 2020, I am still working on this one.
 """
 
 
@@ -18,19 +18,23 @@ while True == True:
         continue
     else:
         movies = []
+        def AddMovie():
+            title = input("Enter the movie title: ")
+            director = input("Enter the movie director: ")
+            year = input("Enter the movie release year: ")
+            movies.append({'title': title, 'director': director, 'year': year})
+            print(f"You've added: ")
+            print(f"""movie "{title}",""")
+            print(f"""director "{director}",""")
+            print(f"""released "{year}".""")
+            print(movies)
         while True == True:
             MENU_PROMPT = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: ")
             if MENU_PROMPT == "q":
                 print("Program will now exit.")
                 quit()
             elif MENU_PROMPT == "a":
-                title = input("Enter the movie title: ")
-                director = input("Enter the movie director: ")
-                year = input("Enter the movie release year: ")
-
-                movies.append({'title': title, 'director': director, 'year': year})
-
-                print(movies)
+                AddMovie()
             elif MENU_PROMPT == "l":
                 pass
             elif MENU_PROMPT == "f":
